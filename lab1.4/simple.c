bac1a8af7de4e0f08438ec1ca995f7bbd334e216
oscreader <oscreader@OSC>
2018-08-17 07:59:00 -0600

    Add

--- lab1.4/simple.c
+++ lab1.4/simple.c
@@ -1,12 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/jiffies.h>
+#include <linux/utsname.h>
 
+
+void uptime(void);
+void getSystemInfo(void);
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
-
+	uptime();
+	getSystemInfo();
        return 0;
 }
 
@@ -14,6 +20,26 @@ int simple_init(void)
 void simple_exit(void) {
 	printk(KERN_INFO "Removing Module\n");
 }
+void uptime() {
+	printk("Uptime: %i\n", jiffies_to_msecs(get_jiffies_64())/1000);
+}
+void getSystemInfo() {
+	struct new_utsname *buf;
+	buf = utsname();
+
+	printk("Nodename: %s\n", buf->nodename);
+	printk("Machine %s\n" , buf-> machine);
+	printk("Sysname: %s\n", buf-> sysname);
+	printk("Release: %s\n", buf->release);
+	printk("Version: %s\n", buf->version);
+#ifdef __linux__
+	printk("Domain Name: %s\n", buf->domainname);
+#endif
+} 
+ /*void Student(STUDENT) {
+	int c, age;
+	char[] name;  */ 
+	
 
 /* Macros for registering module entry and exit points. */
 module_init( simple_init );
