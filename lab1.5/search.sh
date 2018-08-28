6ee9fe6f0af3caca05cbb7eae4d137e0f13fff2f
JennyJockenhoefer <42178370+JennyJockenhoefer@users.noreply.github.com>
2018-08-28 09:27:03 -0500

    Update search.sh

--- lab1.5/search.sh
+++ lab1.5/search.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-#first
+#!/bin/bash  
+directory=$1 
+echo $directory #path user is giving
 
-for dir in $(find -type d );
+for dir in $(find $directory -type d );
   do
     echo " .h files in " $dir; 
   ls $dir/* .h; 
  done
- 
-for dir in $(find -type d );
+
+for dir in $(find $directory -type d );
   do 
     echo " .c files in " $dir; 
   ls $dir/* .c; 
   
   done
+  
