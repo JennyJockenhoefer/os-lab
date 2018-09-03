e8d0c6a7528d635ebcab8f3f7df3505a448ac34d
oscreader <oscreader@OSC>
2018-09-03 17:24:27 -0600

    neckerich

--- lab1.6/UnixShell.sh
+++ lab1.6/UnixShell.sh
@@ -10,7 +10,28 @@ int main(void) {
     printf("osh>"); 
     fflush(stdout);
     
-  
+	char* args [3];  //sets up array mit 3 Stellen
+	string ls = "ls";
+	args[0] = "ps"//array an der stelle 0
+	args[2] = NULL; //array an der stelle 1 endet hier 
+        args[1] = "-ael"
+   pid = fork();
+   if(pid < 0) {
+   perror("fork");
+    if(pid == 0) {   //kid
+	if (execvp(argv[0], args ) == <0)
+	{ perror ("exec");
+	}
+   }
+
+	
+    if(pid == &) {
+	wait(0);
+	cout << "parend:" << pld << endl; 
+}
+	return 0;
+
+	
 /** 
 * After reading user input, the steps are: 
 * (1) fork a child process using fork() 
